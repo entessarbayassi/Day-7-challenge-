958579c6e14230e253ed8e951e95f3e2426b867c
entessarbayassi <entessarbayassi@gmail.com>
2025-08-20 21:58:13 +0300

    updated

---
+++ script.js
@@ -0,0 +1,12 @@
+
+
+
+let name = prompt("Enter your name");;
+let age = prompt("Enter your age");
+const city = "sadadiya";
+
+console.log("%c"+ name ,"color: green");
+console.log("%c"+ age ,"color: green");
+console.log("%c"+ city ,"color: green");
+
+
